c2d13213199f76e8c344b90185833c6bf063476c
Mengli Ding <mlding@thoughtworks.com>
2017-07-31 12:03:40 +0800

    add fourth step post student API

--- expressServer.js
+++ expressServer.js
@@ -8,11 +8,11 @@ const client = redis.createClient('6379', '127.0.0.1');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + '/'));
 
 let accessCount = 0;
 app.get('/', (req, res) => {
-  // res.send('hello world');
   res.sendFile(__dirname + 'index.html');
   accessCount++;
   client.set('accessCount', accessCount, redis.print);
@@ -29,30 +29,30 @@ app.post('/student', (req, res) => {
    });
   const inputStr = inputData.join(", ");
   const isInputStrValid = StudentModule.Student.validateStudentString(inputStr);
+  // client.flushdb( function (err, succeeded) {
+  //     console.log(succeeded); // will be true if successfull
+  // });
 
-  const allStudentInfo = JSON.parse(client.hgetall('allStudentInfo', function(err, allStudentInfo) {
-    console.log(allStudentInfo);
-  }));
-
-  let isDuplicateNumStudent = false;
-  if(allStudentInfo) {
-      isDuplicateNumStudent = Object.keys(allStudentInfo).some(studentNum => {
-          return studentNum === inputData[1];
-      });
-  }
-
-  if(isInputStrValid && !isDuplicateNumStudent) {
-    const student = StudentModule.Student.initStudentFromString(inputStr);
-    const stuNum = student.num;
-
-    client.hmset('allStudentInfo', {
-        `${stuNum}`: JSON.stringify(student)
-    });
-
-    res.send(student);
-  } else {
-      res.send('404');
-  }
+  let key = `stu:${inputData[1]}`;
+  client.get(key, (err, student) => {
+      if(isInputStrValid) {
+          if(student == null) {
+              const student = StudentModule.Student.initStudentFromString(inputStr);
+              client.set(key, JSON.stringify(student));
+              res.send(student);
+          } else {
+              res.status(400).send({
+                  statusCode: 400,
+                  errMsg: "该学号已存在"
+              })
+          }
+      } else {
+          res.status(400).send({
+              statusCode: 400,
+              errMsg: "请输入正确的格式..."
+          })
+      }
+  });
 });
 
 app.listen(3000, () => {
