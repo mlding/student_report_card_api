95960f2256ac16b75093d15707e2717aa08fe84c
Mengli Ding <mlding@thoughtworks.com>
2017-07-31 21:26:11 +0800

    finish update and delete student info function

--- expressServer.js
+++ expressServer.js
@@ -1,14 +1,12 @@
 const redis = require('redis');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const StudentModule = require('./src/student')
 
 const client = redis.createClient('6379', '127.0.0.1');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname + '/'));
 
 let accessCount = 0;
@@ -57,15 +55,11 @@ app.get('/students', (req, res) => {
         });
     }
   } else {
-    res.status(400).send({
-        statusCode: 400,
-        errMsg: '请按正确的格式输入查询字符串'
-    })
+    res.status(400).send('请按正确的格式输入查询字符串')
   }
 });
 
-
-app.post('/student', (req, res) => {
+app.post('/addStudent', (req, res) => {
   const inputAttr = ["inputName", "inputNum", "ethnic", "inputClass",
      "inputMandarinScore", "inputMathScore", "inputEnglishScore", "inputProgrammingScore"];
   const inputData = inputAttr.map(ele => {
@@ -73,9 +67,6 @@ app.post('/student', (req, res) => {
    });
   const inputStr = inputData.join(", ");
   const isInputStrValid = StudentModule.Student.validateStudentString(inputStr);
-  // client.flushdb( function (err, succeeded) {
-  //     console.log(succeeded); // will be true if successfull
-  // });
 
   let key = `stu:${inputData[1]}`;
   client.get(key, (err, student) => {
@@ -85,17 +76,50 @@ app.post('/student', (req, res) => {
               client.set(key, JSON.stringify(student));
               res.send(student);
           } else {
-              res.status(400).send({
-                  statusCode: 400,
-                  errMsg: '该学号已存在'
-              })
+              res.status(400).send('该学号已存在')
           }
       } else {
-          res.status(400).send({
-              statusCode: 400,
-              errMsg: '请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,…）'
-          })
+          res.status(400).send('请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,…）')
       }
+  });
+});
+
+app.put('/students/:id', (req, res) => {
+  const stuNum = req.params.id;
+  const inputObj = req.body;
+  const inputData = [inputObj.alterName, inputObj.alterNum, inputObj.alterEthnic, inputObj.alterClass,
+    inputObj.alterMandarinScore, inputObj.alterMathScore, inputObj.alterEnglishScore, inputObj.alterProgrammingScore];
+  const inputStr = inputData.join(", ");
+  const isInputStrValid = StudentModule.Student.validateStudentString(inputStr);
+
+  let key = `stu:${stuNum}`;
+  client.get(key, () => {
+    if(isInputStrValid) {
+        const student = StudentModule.Student.initStudentFromString(inputStr);
+        client.set(key, JSON.stringify(student));
+        res.send(student);
+    } else {
+        res.status(400).send('请按正确的格式输入要修改的学生的信息（格式： 学号, 学号,…）')
+    }
+  });
+});
+
+app.delete('/students/:id', (req, res) => {
+  const stuNum = req.params.id;
+  let key = `stu:${stuNum}`;
+  client.get(key, (err, reply) => {
+    if(reply) {
+      client.del(key);
+      res.send({
+        statusCode: 200,
+        msg: "该学生已成功删除"
+      });
+    } else {
+      res.send({
+        statusCode: 400,
+        msg: "该学生不存在"
+      });
+    }
   });
 });
 
--- static/bundle.js
+++ static/bundle.js
@@ -60,7 +60,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "75ddb9d0120536d63051"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "772810109dbbde46549b"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -727,31 +727,17 @@ function fixedTwoDecimal(number) {
     return number.toFixed(2);
 }
 
-/* function addStudentInfo(inputAttr, isAlter) {
-    const inputData = inputAttr.map(ele => {
-       return $(ele).val();
-     });
-    const inputStr = inputData.join(", ");
-    const isInputStrValid = StudentModule.Student.validateStudentString(inputStr);
-
-    const allStudentInfo = getAllStudentsInfo();
-    const isDuplicateNumStudent = allStudentInfo.some(studentEle => {
-      return studentEle.num === inputData[1];
-    });
-
-    let msg = '请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...)';
-    let msgType = 'warning';
-    if(!isAlter && isDuplicateNumStudent) {
-      msg = '您输入的学生学号重复，请重新输入';
-    } else if(isInputStrValid) {
-      let student = StudentModule.Student.initStudentFromString(inputStr);
-      student.save();
-      $("form").trigger("reset");
-      msg = '已成功添加该学生成绩';
-      msgType = 'success';
-    }
-    $(".btns").before(`<div class="${msgType} msg">${msg}</div>`);
-}*/
+function fillStudentInfo(stuInfo) {
+  const { name, num, ethnic, klass, mandarinScore, mathScore, englishScore, programmingScore } = stuInfo;
+  $("#alterModal #alterName").val(name);
+  $("#alterModal #alterNum").val(num);
+  $("#alterModal #alterEthnic").val(ethnic);
+  $("#alterModal #alterClass").val(klass);
+  $("#alterModal #alterMandarinScore").val(mandarinScore);
+  $("#alterModal #alterMathScore").val(mathScore);
+  $("#alterModal #alterEnglishScore").val(englishScore);
+  $("#alterModal #alterProgrammingScore").val(programmingScore);
+}
 
 $("#queryTab").click(function() {
   $("#numList").val('');
@@ -762,11 +748,12 @@ $("#allTab").click(function(e) {
   e.preventDefault();
   let studentsInfoStr = '';
   $("table tr td").remove();
-  // allStudentInfo.forEach(studentInfo => {
-  //    studentsInfoStr += `<tr><td>${studentInfo.name}</td><td>${studentInfo.num}</td><td>${studentInfo.ethnic}</td><td>${studentInfo.klass}</td><td>${studentInfo.mandarinScore}</td><td>${studentInfo.mathScore}</td><td>${studentInfo.englishScore}</td><td>${studentInfo.programmingScore}</td><td>${fixedTwoDecimal(studentInfo.averageScore)}</td><td>${fixedTwoDecimal(studentInfo.totalScore)}</td</tr>`;
-  // });
-
-  $("table").append(studentsInfoStr);
+  $.get(`/students?queryStr=`, {}, allStudentInfo => {
+    allStudentInfo.forEach(studentInfo => {
+       studentsInfoStr += `<tr><td>${studentInfo.name}</td><td>${studentInfo.num}</td><td>${studentInfo.ethnic}</td><td>${studentInfo.klass}</td><td>${studentInfo.mandarinScore}</td><td>${studentInfo.mathScore}</td><td>${studentInfo.englishScore}</td><td>${studentInfo.programmingScore}</td><td>${fixedTwoDecimal(studentInfo.averageScore)}</td><td>${fixedTwoDecimal(studentInfo.totalScore)}</td</tr>`;
+    });
+    $("table").append(studentsInfoStr);
+  });
 });
 
 $("#queryBtn").click(function(e) {
@@ -774,51 +761,46 @@ $("#queryBtn").click(function(e) {
   e.preventDefault();
   const numsQueryStr = $("#numList").val();
 
-  let request = new XMLHttpRequest();
-  request.open('get', BASE_URL + `/students?queryStr=${numsQueryStr}`);
   let domStr = '';
-  request.onreadystatechange = function () {
-      if (request.readyState == 4) {
-          const students = JSON.parse(request.responseText);
-          if (request.status == 200) {
-            if(students.statusCode == 400) {
-            } else {
-              const response = `${students.map((student) => {
-                return `<tr><td>${student.name}</td><td>${student.num}</td><td>${student.ethnic}</td><td>${student.klass}</td><td>${student.mandarinScore}</td><td>${student.mathScore}</td><td>${student.englishScore}</td><td>${student.programmingScore}</td><td>${fixedTwoDecimal(student.averageScore)}</td><td>${fixedTwoDecimal(student.totalScore)}</td><td><a class="alterLink" href="#" data-stu-num="${student.num}">修改</a></td><td><a class="deleteLink" href="#" data-stu-num="${student.num}">删除</a></td></tr>`;
-              })}
-          <tr><td>全班总分平均数：${fixedTwoDecimal(StudentModule.Student.averageOfTotalScoreSum(students))}</td></tr>
-          <tr><td>全班总分中位数：${fixedTwoDecimal(StudentModule.Student.medianOfTotalScoreSum(students))}</td></tr></table>`;
-              domStr = response;
-            }
-          } else {
-              domStr = students.errMsg;
-          }
-        }
+  $.get(`/students?queryStr=${numsQueryStr}`, {}, students => {
+    const response = `${students.map((student) => {
+      return `<tr><td>${student.name}</td><td>${student.num}</td><td>${student.ethnic}</td><td>${student.klass}</td><td>${student.mandarinScore}</td><td>${student.mathScore}</td><td>${student.englishScore}</td><td>${student.programmingScore}</td><td>${fixedTwoDecimal(student.averageScore)}</td><td>${fixedTwoDecimal(student.totalScore)}</td><td><a class="alterLink" href="#" data-stu-num="${student.num}">修改</a></td><td><a class="deleteLink" href="#" data-stu-num="${student.num}">删除</a></td></tr>`;
+    })}
+<tr><td>全班总分平均数：${fixedTwoDecimal(StudentModule.Student.averageOfTotalScoreSum(students))}</td></tr>
+<tr><td>全班总分中位数：${fixedTwoDecimal(StudentModule.Student.medianOfTotalScoreSum(students))}</td></tr></table>`;
+    domStr = response;
+    $("table").append(domStr);
+  });
 
-      $("table").append(domStr);
-  };
-  request.setRequestHeader("Content-Type", "application/json");
-  request.send();
 });
 
 $("table").on("click", ".alterLink", function() {
   $("#alterModal").modal("show");
   const stuNum = $(this).data("stu-num");
-  const stuInfo = JSON.parse(localStorage.getItem(stuNum));
-  const { name, num, ethnic, klass, mandarinScore, mathScore, englishScore, programmingScore } = stuInfo;
-  $("#alterModal #alterName").val(name);
-  $("#alterModal #alterNum").val(num);
-  $("#alterModal #alterEthnic").val(ethnic);
-  $("#alterModal #alterClass").val(klass);
-  $("#alterModal #alterMandarinScore").val(mandarinScore);
-  $("#alterModal #alterMathScore").val(mathScore);
-  $("#alterModal #alterEnglishScore").val(englishScore);
-  $("#alterModal #alterProgrammingScore").val(programmingScore);
+  $.get(`/students?queryStr=${stuNum}`, {}, students => {
+    fillStudentInfo(students[0]);
+  });
 
   $("#alterStudentInfoBtn").click(function() {
-    const inputAttr = ["#alterName", "#alterNum", "#alterEthnic", "#alterClass",
-       "#alterMandarinScore", "#alterMathScore", "#alterEnglishScore", "#alterProgrammingScore"];
-    addStudentInfo(inputAttr, true);
+    const inputObj = {
+      'alterName': $('#alterName').val(),
+      'alterNum': $('#alterNum').val(),
+      'alterEthnic': $('#alterEthnic').val(),
+      'alterClass': $('#alterClass').val(),
+      'alterMandarinScore': $('#alterMandarinScore').val(),
+      'alterMathScore': $('#alterMathScore').val(),
+      'alterEnglishScore': $('#alterEnglishScore').val(),
+      'alterProgrammingScore': $('#alterProgrammingScore').val()
+    };
+
+    $.ajax({
+      type: 'PUT',
+      data: inputObj,
+      url:  BASE_URL + '/students/' + stuNum,
+      dataType: 'JSONP'
+    })
+
+    $("#queryBtn").trigger('click');
   });
 });
 
@@ -827,7 +809,13 @@ $("table").on("click", ".deleteLink", function() {
   const deleteStuNum = $(this).data("stu-num");
 
   $("#deleteStudentInfoBtn").click(function() {
-    localStorage.removeItem(deleteStuNum);
+    $.ajax({
+      type: 'DELETE',
+      url:  BASE_URL + '/students/' + deleteStuNum,
+      dataType: 'JSONP'
+    });
+
+    $("#queryBtn").trigger('click');
   });
 });
 
